d46b120b16612019d009f8a99c989bb12c27a2d2
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-04-21 14:32:22 -0400

    adds seed data for testing purposes

---
+++ app/models/seed.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const Place = require('./place')
+
+const db = require('../../config/db')
+
+const starterOutfits = [
+    {
+        description: "cute versatile look for spring",
+        date: "2022-04-21",
+        img: "https://i.imgur.com/Mkcjobx.jpg",
+        rating: "",
+        tags: ["spring looks", "date night", "casual"],
+    },
+    {
+        description: "cool look for a cool person",
+        date: "2022-04-21",
+        img: "https://i.imgur.com/vze52a7.jpg",
+        rating: "",
+        tags: ["punk", "stylish", "trendy"],
+    },
+    {
+        description: "just vibing",
+        date: "2022-04-21",
+        img: "https://i.imgur.com/mRnJtLu.jpg",
+        rating: "",
+        tags: ["casual", "simple", "chic"],
+    },
+]
+
+// first we connect to the db via mongoose
+mongoose.connect(db, {
+    useNewUrlParser: true,
+})
+    .then(() => {
+        // then we remove all the places
+        Outfit.deleteMany({ owner: null })
+            .then(deletedOutfits => {
+                console.log('deleted outfits', deletedOutfits)
+                // then we create using the starterOutfits array
+                // we'll use console logs to check if it's working or if there are errors
+                Outfit.create(starterOutfits)
+                    .then(newOutfits => {
+                        console.log('the new outfits', newOutfits)
+                        mongoose.connection.close()
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        mongoose.connection.close()
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+                mongoose.connection.close()
+            })
+    })
+    // then at the end, we close our connection to the db
+    .catch(error => {
+        console.log(error)
+        mongoose.connection.close()
+    })
